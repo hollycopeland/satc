03808bf7b6e2ad73bf8dfec629ed80d535fd0673
Katie Broida <katie.broida@upstatement.com>
2018-03-09 16:09:57 -0500

    Hook up sexy girls updates

--- app.js
+++ app.js
@@ -12,6 +12,11 @@ var SATC = {
     this.seasonEl = document.getElementById('season');
     this.episodeEl = document.getElementById('episode');
     this.titleEl = document.getElementById('title');
+    this.carrieEl = document.getElementById('carrieUpdate');
+    this.mirandaEl = document.getElementById('mirandaUpdate');
+    this.charlotteEl = document.getElementById('charlotteUpdate');
+    this.samanthaEl = document.getElementById('samanthaUpdate');
+
 
     Tabletop.init( { key: this.sheetUrl, callback: this.showInfo.bind( this ) } );
   },
@@ -21,9 +26,14 @@ var SATC = {
     this.yourCloset = tabletop.sheets('Your Closet').all();
     let epSuggestion = this.getRandomEp( this.SATCdata );
 
+    console.log(epSuggestion);
     this.seasonEl.innerText = epSuggestion.season;
     this.episodeEl.innerText = epSuggestion.episode;
     this.titleEl.innerText = epSuggestion.title;
+    this.carrieEl.innerText = epSuggestion.carrieUpdate;
+    this.mirandaEl.innerText = epSuggestion.mirandaUpdate;
+    this.charlotteEl.innerText = epSuggestion.charlotteUpdate;
+    this.samanthaEl.innerText = epSuggestion.samanthaUpdate;
   },
 
   getRandomEp( data ) {
