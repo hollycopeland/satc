99c09312e11b5abf270dcdf1fd799b06b263a5ce
Katie Broida <katie.broida@upstatement.com>
2018-03-09 15:52:27 -0500

    Add JS to hook up random episode suggestions

---
+++ app.js
@@ -0,0 +1,38 @@
+document.addEventListener( 'DOMContentLoaded', function() {
+  SATC.init();
+} );
+
+var SATC = {
+  
+  SATCdata: [],
+  yourCloset: [],
+  sheetUrl: 'https://docs.google.com/spreadsheets/d/18GUQnDV9AX32KPacE9q8xEZtrOqJ6qjTV1C8eQfAdoE/edit?usp=sharing',
+  
+  init: function() {
+    this.seasonEl = document.getElementById('season');
+    this.episodeEl = document.getElementById('episode');
+    this.titleEl = document.getElementById('title');
+
+    Tabletop.init( { key: this.sheetUrl, callback: this.showInfo.bind( this ) } );
+  },
+
+  showInfo: function( data, tabletop ) {
+    this.SATCdata = tabletop.sheets('Base List').all();
+    this.yourCloset = tabletop.sheets('Your Closet').all();
+    let epSuggestion = this.getRandomEp( this.SATCdata );
+
+    console.log(epSuggestion);
+    this.seasonEl.innerText = epSuggestion.season;
+    this.episodeEl.innerText = epSuggestion.episode;
+    this.titleEl.innerText = epSuggestion.title;
+  },
+
+  getRandomEp( data ) {
+    var maxEp = data.length;
+    var epIndex = Math.floor( Math.random() * maxEp );
+
+    return data[ epIndex ];
+  },
+
+}
+
